fde51a528b69d774f28ecccb120dba6b5054ebcf
PythonAkoto <yawakoto.python@example.com>
2024-06-28 22:26:08 +0100

    survey setup - returns json

--- pythonakato/main.py
+++ pythonakato/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flaskwebgui import FlaskUI
 import ssl
 ssl.create_default_context = ssl._create_unverified_context
@@ -11,7 +11,30 @@ def home():
     if request.method == 'GET':
         return render_template("index.html")
     else:
-        return 'message sent!'
+        track_name = request.form.get('track_name')
+        album_name = request.form.get('album_name')
+        artist_name = request.form.get('artist_name')
+        audio_year = request.form.get('audio_year')
+        audio_genre = request.form.get('audio_genre')
+        album_type = request.form.get('album_type')
+        release_type = request.form.get('release_type')
+        audio_frame_rate = request.form.get('audio_frame_rate')
+        audio_notes = request.form.get('audio_notes')
+
+        data = {'File Name': 
+            {
+                'Track Name': track_name,
+                'Artist': artist_name,
+                'Album Name': album_name,
+                'Genre': audio_genre,
+                'Year': audio_year,
+                'Frame Rate': audio_frame_rate,
+                'Album Type': album_type,
+                'Release Type': release_type,
+                'Audio Notes': audio_notes,
+            }
+        }
+        return jsonify(data)
 
 if __name__ == "__main__":
     debug = False
